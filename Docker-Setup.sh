1f650f5c9d72a18a14048ecfbd0881b5a7ea33fb
Burak Oruk <91758706+BurakOruk26@users.noreply.github.com>
2024-11-23 21:45:32 +0300

    Dockerfile changed to be used with FastAPI
    Conda environment is added to the path at the Dockerfile so that FastAPI app can be run through shell.
    Also a small script for testing the FastAPI has been added. There is an issue that requires multiple shells to be open via docker-compose. Will be solved!

---
+++ Docker-Setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Change directory to .devcontainer
+cd .devcontainer
+
+echo "Don't forget to run your Docker engine!"
+
+echo "Starting to build the Docker image..."
+docker build -t surdur-image . || { echo "Failed to build the image"; exit 1; }
+
+echo "Built the Docker image."
+
+echo "Composing the image up..."
+docker compose up || { echo "Failed to compose up"; exit 1; }
